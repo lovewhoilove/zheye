8306efadb2b8a2a4ce2ba32d573e2d5c07c9a39e
lovewhoilove <3528300287@qq.com>
2022-11-15 17:11:55 +0800

    自定义下拉菜单组件

---
+++ src/components/MyDropdown.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="dropdown">
+    <a
+      href="#"
+      class="btn btn-outline-light my-2 dropdown-toggle"
+      @click.prevent="toggleOpen"
+    >
+      {{title}}
+    </a>
+    <ul
+      v-if="isOpen"
+      class="dropdown-menu"
+      :style="{display:'block'}"
+    >
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  ref
+} from 'vue'
+
+export default defineComponent({
+  name: 'MyDropdown',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup () {
+    const isOpen = ref(false)
+    const toggleOpen = () => {
+      isOpen.value = !isOpen.value
+    }
+    return {
+      isOpen,
+      toggleOpen
+    }
+  }
+})
+</script>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -11,7 +11,17 @@
     </ul>
     <ul v-else class="list-inline mb-0">
       <li class="list-inline-item">
-        <a href="#" class="btn btn-outline-light my-2">你好 {{user.name}}</a>
+        <MyDropdown :title="`欢迎你 ${user.name}`">
+          <DropdownItem>
+            <a href="#" class="dropdown-item">新建文章</a>
+          </DropdownItem>
+          <DropdownItem disabled>
+            <a href="#" class="dropdown-item">编辑资料</a>
+          </DropdownItem>
+          <DropdownItem>
+            <a href="#" class="dropdown-item">退出登录</a>
+          </DropdownItem>
+        </MyDropdown>
       </li>
     </ul>
   </nav>
@@ -19,6 +29,8 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import MyDropdown from './MyDropdown.vue'
+import DropdownItem from './DropdownItem.vue'
 
 export interface UserProps {
   isLogin: boolean;
@@ -27,7 +39,11 @@ export interface UserProps {
 }
 
 export default defineComponent({
-  name: 'GlobalHeader',
+  name: 'MyHeader',
+  components: {
+    MyDropdown,
+    DropdownItem
+  },
   props: {
     user: {
       type: Object as PropType<UserProps>,
